49e60ff773516d558cc524ce90400d65b1bd47dd
AmoInno <amogelangmashishi12@gmail.com>
2024-11-22 09:42:34 +0200

    testing tailwindcss

--- podcast-app/src/App.tsx
+++ podcast-app/src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="text-5xl font-bold underline">Hello world!</h1>
+      <h1 className="text-5xl font-bold underline text-rose-700">
+        Hello world!
+      </h1>
     </>
   );
 }
